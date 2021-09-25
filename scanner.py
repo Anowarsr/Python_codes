9fdd58b0ea451cdc5c4e46cf68d3aa6a675f230b
Anowar Hossain Sardar <78751929+Anowarsr@users.noreply.github.com>
2021-09-25 07:46:08 -0700

    Add files via upload

---
+++ scanner.py
@@ -0,0 +1,6 @@
+scanner="{} {} {} {}"
+
+print(scanner.format(1,2,3,4))
+print(scanner.format("A","S","I","F"))
+print(scanner.format(True,False,False,True))
+print(scanner.format(scanner,scanner,scanner,scanner))
