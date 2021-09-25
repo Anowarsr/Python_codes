5ece7c7fa3ab61ab69a2dbdaef4b8f1b0f84d092
Anowar Hossain Sardar <78751929+Anowarsr@users.noreply.github.com>
2021-09-24 21:19:28 -0700

    Add files via upload

---
+++ while-forloop.py
@@ -0,0 +1,12 @@
+i=0
+num=[]
+while i<6:
+    print(f"At the top i is {i}")
+    num.append(i)
+    i=i+1
+    print("numbers row:",num)
+    print(f"At the bottom i is {i}")
+
+print("The numbers" )
+for numbers in num:
+    print(numbers)
---
+++ whileloop.py
@@ -0,0 +1,9 @@
+i=0
+num=[]
+while i<6:
+    print(f"At the top i is {i}")
+    num.append(i)
+    i=i+1
+    print("numbers row:",num)
+    print(f"At the bottom i is {i}")
+
