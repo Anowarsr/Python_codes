d4743e110f092f9eb6138e8b882dfa8fdc16d1f2
Anowar Hossain Sardar <78751929+Anowarsr@users.noreply.github.com>
2021-09-25 06:42:34 -0700

    Update filehandle.py

--- filehandle.py
+++ filehandle.py
@@ -3,9 +3,10 @@ from sys import argv
 script,User_name = argv
 promt = '>'
 
-print(f"Hi{User_name}.I'm good in {script}")
+print(f"Hi I'm {User_name}.I'm good in {script}")
 print(f"You Know my name!{User_name}")
 
+print("Just put a name: ")
 tags = input(promt)
 
 print("Which type of laptop you have?")
@@ -13,7 +14,7 @@ laptop = input(promt)
 print("Where is your Hometown?")
 hometown = input(promt)
 
-print(f'''okay, you said {tags} knowabout me, 
-currently yo hava {laptop} laptop 
-and you said me your hometown is {hometown}.
-thanks to talking me Great Day''')
+print(f'''Okay!! you said that {tags} know about me, 
+currently you have {laptop} laptop 
+and you said to me that your hometown is {hometown}.
+It is a great,have a good day!!''')
